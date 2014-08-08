fe48b0acc253b2c7f8e4955f808968522f7f0e3d
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-08-08 17:09:42 +0200

    Use decimal instead of float for zero

--- abreviated_journal.py
+++ abreviated_journal.py
@@ -162,8 +162,8 @@ class AbreviatedJournalReport(JasperReport):
                             'period_date': period.start_date,
                             'code': account.code,
                             'name': account.name,
-                            'debit': 0.0,
-                            'credit': 0.0,
+                            'debit': Decimal('0.0'),
+                            'credit': Decimal('0.0'),
                         })
         return res, parameters
 
