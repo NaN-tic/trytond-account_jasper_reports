8802875fa0a0ef84d7baba99d7ab3aa2427bf4ff
Bernat Brunet Torruella <bernat@nan-tic.com>
2017-05-25 16:11:19 +0200

    In journal, make that search the active or not active accounts when create opne/close move. Change the way that search debit/credit/balance on accounts when exclude party moves

--- journal.py
+++ journal.py
@@ -286,6 +286,7 @@ class JournalReport(JasperReport):
                 accounts = Account.search([
                         ('parent', '!=', None),
                         ('kind', '!=', 'view'),
+                        ('active', 'in', [True, False]),
                         ])
                 parties = Party.search([
                         ('active', 'in', [True, False]),
