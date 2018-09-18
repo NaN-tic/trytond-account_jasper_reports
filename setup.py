9c43fed7bf4c754d9171624bdca9a12e7078f85e
Sergio Morillo <sergio.morillo@datalife.com.es>
2018-09-18 13:32:59 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,10 @@ except ImportError:
 
 MODULE = 'account_jasper_reports'
 PREFIX = 'trytonspain'
-MODULE2PREFIX = {}
+MODULE2PREFIX = {
+    'jasper_reports': 'trytonspain',
+    'account_invoice_company_currency': 'trytonspain'
+}
 
 
 def read(fname):
@@ -51,7 +54,25 @@ for dep in info.get('depends', []):
 requires.append(get_require_version('trytond'))
 
 tests_require = [get_require_version('proteus')]
-dependency_links = []
+series = '%s.%s' % (major_version, minor_version)
+if minor_version % 2:
+    branch = 'default'
+else:
+    branch = series
+dependency_links = [
+    ('hg+https://bitbucket.org/trytonspain/'
+        'trytond-jasper_reports@%(branch)s'
+        '#egg=trytonspain_jasper_reports-%(series)s' % {
+            'branch': branch,
+            'series': series,
+            }),
+    ('hg+https://bitbucket.org/trytonspain/'
+        'trytond-account_invoice_company_currency@%(branch)s'
+        '#egg=trytonspain_account_invoice_company_currency-%(series)s' % {
+            'branch': branch,
+            'series': series,
+            }),
+    ]
 if minor_version % 2:
     # Add development index for testing with proteus
     dependency_links.append('https://trydevpi.tryton.org/')
