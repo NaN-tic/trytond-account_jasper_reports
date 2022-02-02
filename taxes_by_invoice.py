2eef2d119f2800531e112f8d30f6b71c7c54a376
Bernat Brunet <bernat@nan-tic.com>
2022-02-02 14:47:17 +0100

    Add partner type in taxes by invoice reports to only show in differnet way the supplier cancelled invoices, not the partners.
    Task #049097

--- taxes_by_invoice.py
+++ taxes_by_invoice.py
@@ -222,6 +222,7 @@ class TaxesByInvoiceReport(JasperReport):
         parameters['company_vat'] = (company
             and company.party.tax_identifier and
             company.party.tax_identifier.code) or ''
+        parameters['partner_type'] = data.get('partner_type')
 
         domain = [
             ('invoice.move', '!=', None),
