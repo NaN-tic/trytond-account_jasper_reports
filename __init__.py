13c3bb682705c99f59d4d4f9310c332daca6675a
juanjo-nan <57089222+juanjo-nan@users.noreply.github.com>
2023-03-03 15:15:54 +0100

    * Remove general ledger report and taxes by invoice reports (Taxes by Invoice and Taxes by Invoice and Period). (#4)
    Add dependency with account_reports.
    Delete tests form unused reports.
    
    Task #071305

--- __init__.py
+++ __init__.py
@@ -2,11 +2,9 @@
 # copyright notices and license terms.
 from trytond.pool import Pool
 from . import common
-from . import general_ledger
 from . import abreviated_journal
 from . import journal
 from . import trial_balance
-from . import taxes_by_invoice
 
 
 def register():
@@ -15,23 +13,16 @@ def register():
         common.Party,
         journal.PrintJournalStart,
         abreviated_journal.PrintAbreviatedJournalStart,
-        general_ledger.PrintGeneralLedgerStart,
         trial_balance.PrintTrialBalanceStart,
-        taxes_by_invoice.PrintTaxesByInvoiceAndPeriodStart,
         common.FiscalYear,
         module='account_jasper_reports', type_='model')
     Pool.register(
         journal.PrintJournal,
         abreviated_journal.PrintAbreviatedJournal,
-        general_ledger.PrintGeneralLedger,
         trial_balance.PrintTrialBalance,
-        taxes_by_invoice.PrintTaxesByInvoiceAndPeriod,
         module='account_jasper_reports', type_='wizard')
     Pool.register(
         journal.JournalReport,
         abreviated_journal.AbreviatedJournalReport,
-        general_ledger.GeneralLedgerReport,
         trial_balance.TrialBalanceReport,
-        taxes_by_invoice.TaxesByInvoiceReport,
-        taxes_by_invoice.TaxesByInvoiceAndPeriodReport,
         module='account_jasper_reports', type_='report')
