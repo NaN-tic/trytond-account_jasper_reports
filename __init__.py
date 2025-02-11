51d0b254ce6fa25d0972903e5f61e7f24bd29afe
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2025-02-11 13:37:38 +0100

    Remove trial_balance report, now adapted to html in account_report module.
    Task #171808

--- __init__.py
+++ __init__.py
@@ -4,7 +4,6 @@ from trytond.pool import Pool
 from . import common
 from . import abreviated_journal
 from . import journal
-from . import trial_balance
 
 
 def register():
@@ -13,16 +12,13 @@ def register():
         common.Party,
         journal.PrintJournalStart,
         abreviated_journal.PrintAbreviatedJournalStart,
-        trial_balance.PrintTrialBalanceStart,
         common.FiscalYear,
         module='account_jasper_reports', type_='model')
     Pool.register(
         journal.PrintJournal,
         abreviated_journal.PrintAbreviatedJournal,
-        trial_balance.PrintTrialBalance,
         module='account_jasper_reports', type_='wizard')
     Pool.register(
         journal.JournalReport,
         abreviated_journal.AbreviatedJournalReport,
-        trial_balance.TrialBalanceReport,
         module='account_jasper_reports', type_='report')
